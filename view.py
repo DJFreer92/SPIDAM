f03d90040ff3f1f03dac8eee25bb181421248312
Daniel Freer <deletes_minimal.0w@icloud.com>
2023-12-01 17:08:54 -0500

    Created View class
    Created View class with buttons and labels for GUI

---
+++ view.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from tkinter import ttk
+
+class View(ttk.Frame):
+	def __init__(self, parent):
+		super().__init__(parent)
+
+		#create widgets
+		#load audio button
+		self.load_audio_button = ttk.Button(self, text = 'Load Audio', command = self.load_audio_button_clicked)
+		self.load_audio_button.grid(row = 1, column = 1, padx = 10)
+
+		#file name label
+		self.file_name_label = ttk.Label(self, text = '')
+		self.file_name_label.grid(row = 1, column = 2, sticky = tk.W)
+
+		#statistics label
+		self.statistics_label = ttk.Label(self, text = 'Audio Statistics:')
+		self.statistics_label.grid(row = 2, column = 1, sticky = tk.W)
+
+		#audio length label
+		self.audio_length_label = ttk.Label(self, text = 'Audio length: ')
+		self.audio_length_label.grid(row = 3, column = 1, sticky = tk.W)
+
+		#frequency of greatest amplitude label
+		self.frequency_greatest_amplitude_label = ttk.Label(self, text = 'Frequency of greatest amplitude: ')
+		self.frequency_greatest_amplitude_label.grid(row = 4, column = 1, sticky = tk.W)
+
+		#RT60 difference label
+		self.rt60_difference_label = ttk.Label(self, text = 'RT difference: ')
+		self.rt60_difference_label.grid(row = 5, column = 1, sticky = tk.W)
+
+		#TO DO: Plots here
+
+		#next plot button
+		self.next_plot_button = ttk.Button(self, text = 'Next Plot', command = self.next_plot_button_clicked)
+		self.next_plot_button.grid(row = 7, column = 2, padx = 15)
+
+		#set the controller
+		self.controller = None
+
+	def set_controller(self, controller):
+		self.controller = controller
+
+	def load_audio_button_clicked(self):
+		if self.controller:
+			#TO DO: Call to controller to load audio file
+			pass
+
+	def next_plot_button_clicked(self):
+		if self.controller:
+			#TO DO: Switch to next plot
+			pass
+
+	def set_file_name(self, file_name):
+		self.file_name_label['text'] = file_name
