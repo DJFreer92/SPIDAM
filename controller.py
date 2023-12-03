2640fbc54afb8c72aa7f8d2297756c4083f60b54
DevJake123 <89547864+DevJake123@users.noreply.github.com>
2023-12-03 16:18:47 -0500

    updated controller.py

--- controller.py
+++ controller.py
@@ -3,5 +3,11 @@ from view import View
 #from model import Model
 
 #Controller is to be written last
+#class Controller:
+    #def __init__(self, Model, View):
+        #self.model = Model
+        #self.view = View
+    #def Start():
 
-#def Start():
+
+#Start()
