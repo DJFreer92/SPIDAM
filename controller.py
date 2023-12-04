56b521d19c16b1f9f33c6a8432bd21a41ccae6b0
DevJake123 <89547864+DevJake123@users.noreply.github.com>
2023-12-03 22:13:52 -0500

    added load button to controller.py

--- controller.py
+++ controller.py
@@ -1,6 +1,49 @@
 from audio_app import App
 from view import View
 #from model import Model
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+gfile = ''
+# create the root window
+root = tk.Tk()
+root.title('Tkinter Open File Dialog')
+root.resizable(False, False)
+root.geometry('300x150')
+
+def select_file():
+    filetypes = (
+        ('wav files', '*.wav'),
+        ('mp3 files', '*.mp3*')
+        )
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+    gfile = filename
+# tkinter.messagebox — Tkinter message prompts
+    showinfo(
+        title='Selected File',
+        message=filename
+        )
+
+gfile_label = ttk.Label(root, text=gfile)
+gfile_label.pack(side="bottom")
+
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open a File',
+    command=select_file
+    )
+open_button.pack(expand=True)
+# run the application
+root.mainloop()
+
+
+
+
 
 #Controller is to be written last
 #class Controller:
