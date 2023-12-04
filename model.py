fad937a0ba08d78553aa0fbd880ab641bf9591a2
smeares2729 <150273062+smeares2729@users.noreply.github.com>
2023-12-04 01:13:54 -0500

    added model.py

---
+++ model.py
@@ -0,0 +1,8 @@
+class Model:
+
+  def __init__(self):
+    self.audio_file_name = None
+    self.audio_file_path = None
+    self.audio_file_length = 0
+    self.audio_file_frequency_greatest_amplitude = 0
+    self.audio_file_rt60_difference = 0
