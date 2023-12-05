66d947e1712bb11e36dd9d09cae76da82220c40b
DevJake123 <89547864+DevJake123@users.noreply.github.com>
2023-12-05 16:53:03 -0500

    Edited controller.py

--- controller.py
+++ controller.py
@@ -3,14 +3,13 @@ from model import Model
 from tkinter import filedialog as fd
 
 
-#Controller is to be written last
 class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
 
         self.audio_file = ''
-
+    #the specific files that can be chosen to analyze
     def select_audio_file(self):
         filetypes = (
             ('audio files', ('*.wav','*.mp3')),
