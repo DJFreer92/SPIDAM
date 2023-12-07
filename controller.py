7d4632519c1889c63685dc3de489983e0cc3c4dc
DevJake123 <89547864+DevJake123@users.noreply.github.com>
2023-12-07 17:12:45 -0500

    added new function to grab model data from model.py

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 from view import View
 from model import Model
 from tkinter import filedialog as fd
-
+import tkinter as tk
 
 class Controller:
     def __init__(self, model, view):
@@ -25,3 +25,36 @@ class Controller:
 
         if self.audio_file:
             self.model.set_file_path(self.audio_file)
+
+        if self.audio_file:
+            self.model.set_file_path(self.audio_file)
+            self.view.set_file_name(self.audio_file)  # Update file name in the view
+            self.view.set_statistics(length=0, freq_great_amp=0, rt60_diff=0)  # Clear previous statistics
+            self.view.set_plot(None)  # Clear previous plot
+
+    def analyze_and_display(self):
+        file_path = self.file_path_entry.get()
+
+        if file_path:
+            self.model.set_file_path(file_path)
+
+            # Perform analysis using the Model
+            highest_resonance = self.model.compute_highest_resonance()
+            duration = self.model.display_time_value()
+
+            # Update the GUI in View
+            self.view.set_file_name(file_path)
+            self.view.set_statistics(length=duration, freq_great_amp=highest_resonance,rt60_diff=0)  # Replace 0 with actual RT60 difference
+            self.view.set_plot(self.model.waveform())  # Display waveform
+
+
+
+def main():
+    root = tk.Tk()
+    model_instance = Model()
+    view_instance = View(root)
+    controller_instance = Controller(root, model_instance, view_instance)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
