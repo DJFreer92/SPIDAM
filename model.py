a0c4a737c5794010a69f503432098e7185265aed
smeares2729 <150273062+smeares2729@users.noreply.github.com>
2023-12-09 09:48:10 -0500

    Update model.py - Comments added

--- model.py
+++ model.py
@@ -6,17 +6,22 @@ import matplotlib.pyplot as plt
 
 class Model:
     def __init__(self):
+        # initialize all variables needed in self
         self.file_path, self.sample_rate, self.data, self.spectrum, self.freqs, self.t, self.im \
             = '', '', '', '', '', '', ''
 
     def set_values(self, sound):
+        # setting the file path of the sound to be analyzed
         self.file_path = sound.export("Clap.wav", format="wav")
+
+        # sets variables from L26
         self.sample_rate, self.data = sci.io.wavfile.read(self.file_path)
         self.spectrum, self.freqs, self.t, self.im = plt.specgram(self.data, Fs=self.sample_rate, NFFT=1024,
                                                                   cmap=plt.get_cmap('autumn_r'))
         plt.close()
 
     def set_file_path(self, file_path):
+        # if elif clauses to handle wav and mp3, then calling set_values to handle file_path
         if file_path.endswith(".wav"):
             sound = AudioSegment.from_wav(file_path)
             self.set_values(sound)
@@ -25,59 +30,96 @@ class Model:
             self.set_values(sound)
 
     def set_single_channel(self):
+        # simple channel set function
         sound = AudioSegment.from_wav(self.file_path).set_channels(1)
+
+        # handle file_path
         self.set_values(sound)
 
 
     def compute_highest_resonance(self):
+        # opens the wav file for reading to pull signal, then closes it
         with wav.open(self.file_path, 'rb') as audio_file:
             signal_data = np.frombuffer(audio_file.readframes(-1), dtype=np.int16)
+
+        # pull the frequency and power from the signal
         frequencies, power = sci.signal.welch(signal_data, fs=audio_file.getframerate(), nperseg=1024)
-        audio_file.close()
         highest_resonance_index = np.argmax(power)
         highest_resonance_frequency = frequencies[highest_resonance_index]
+
+        # printing the resonance frequency to the third decimal place
         print('%.3f' % highest_resonance_frequency)
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the value of the resonance frequency
         return highest_resonance_frequency
 
     def display_time_value(self):
+        # opens the wav file for reading to get the frames and frame rate, as well as calculating the time of the file
         with wav.open(self.file_path, 'rb') as audio_file:
             frames = audio_file.getnframes()
             rate = audio_file.getframerate()
-        duration = frames / float(rate)
+            duration = frames / float(rate)
+
+        # printing the time to the third decimal place
         print('%.3f' % duration)
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the time of the file
         return duration
 
     def waveform(self):
+        # opens the wav file for reading to pull signal, then closes it
         with wav.open(self.file_path, 'rb') as audio_file:
             signal = np.frombuffer(audio_file.readframes(-1), dtype=np.int16)
+
+        # plot the signal
         plt.figure(figsize=(10, 6))
         plt.plot(signal)
         plt.xlabel('Time')
         plt.ylabel('Amplitude')
         plt.title('Waveform')
         plt.show()
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the signal for possible future use
         return signal
 
     def useful_waveform(self):
+        # opens the wav file for reading to pull signal, then closes it
         with wav.open(self.file_path, 'rb') as audio_file:
             signal = np.frombuffer(audio_file.readframes(-1), dtype=np.int16)
+
+        # cuts signify areas of the signal that represent different frequencies or power
         plt.figure(figsize=(10, 6))
         useful_cut = int(len(signal) * 0.1)
         useful_wave = signal[:useful_cut]
+
+        # plot the signal
         plt.plot(useful_wave)
         plt.xlabel('Time')
         plt.ylabel('Amplitude')
         plt.title('"Useful" Waveform')
         plt.show()
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the modified signal for possible future use
         return signal
 
     def high(self):
+        # opens the wav file for reading to pull signal, then closes it
         with wav.open(self.file_path, 'r') as audio_file:
             signal = np.frombuffer(audio_file.readframes(-1), dtype=np.int16)
+
+        # cuts signify areas of the signal that represent different frequencies or power
         high_cut = int(len(signal) * 0.4)
         high_wave = signal[:high_cut]
         plt.figure(figsize=(10, 6))
@@ -86,36 +128,58 @@ class Model:
         plt.xlabel('Time')
         plt.title('High Waveform')
         plt.show()
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the modified signal for possible future use
         return high_wave
 
     def mid(self):
+        # opens the wav file for reading to pull signal, then closes it
         with wav.open(self.file_path, 'r') as audio_file:
             signal = np.frombuffer(audio_file.readframes(-1), dtype=np.int16)
+
+        # cuts signify areas of the signal that represent different frequencies or power
         high_cut = int(len(signal) * 0.4)
         mid_cut = int(len(signal) * 0.6)
         mid_wave = signal[high_cut:mid_cut]
+
+        # plot the signal
         plt.figure(figsize=(10, 6))
         plt.plot(mid_wave, 'y')
         plt.ylabel('Amplitude')
         plt.xlabel('Time')
         plt.title('Mid Waveform')
         plt.show()
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the modified signal for possible future use
         return mid_wave
 
     def low(self):
+        # opens the wav file for reading to pull signal, then closes it
         with wav.open(self.file_path, 'r') as audio_file:
             signal = np.frombuffer(audio_file.readframes(-1), dtype=np.int16)
+
+        # cuts signify areas of the signal that represent different frequencies or power
         mid_cut = int(len(signal) * 0.6)
         low_wave = signal[mid_cut:]
+
+        # plotting the waveform
         plt.figure(figsize=(10, 6))
         plt.plot(low_wave, 'r')
         plt.ylabel('Amplitude')
         plt.xlabel('Time')
         plt.title('Low Waveform')
         plt.show()
+
+        # reinitialize the file, as wave closes the file once the function is complete
         self.set_file_path('Clap.wav')
+
+        # passing the modified signal for possible future use
         return low_wave
 
     def find_target_frequency(self, freqs):
@@ -158,9 +222,11 @@ class Model:
         rt60 = rt20*3
         plt.show()
         self.set_file_path('Clap.wav')
+        # returns the rt60 time for possible future use
         return abs(rt60)
 
     def rt60_diff(self):
+        # uses code from L26 pptx
         data_in_db = self.frequency_check()
         index_of_max = np.argmax(data_in_db)
         value_of_max = data_in_db[index_of_max]
@@ -174,5 +240,15 @@ class Model:
         rt20 = (self.t[index_of_max_less_5] - self.t[index_of_max_less_25])[0]
         rt60 = rt20*3
         print('%.3f' % abs(rt60))
+        # returns the rt60 time
         return '%.3f' % (abs(rt60) - 0.5)
+
+
+
+
+
+
+
+
+
 
